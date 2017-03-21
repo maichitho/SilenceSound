43534ed34c83f27cc01a4fcced70fa266c4ac707
thomc <thomc@2nf.com.vn>
2017-03-22 00:43:31 +0700

    cm

--- android/app/src/main/java/com/silencesound/MainApplication.java
+++ android/app/src/main/java/com/silencesound/MainApplication.java
@@ -3,7 +3,6 @@ package com.silencesound;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -12,8 +11,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.FacebookSdk;
-import com.facebook.CallbackManager;
 import com.facebook.appevents.AppEventsLogger;
 
  import android.content.pm.PackageInfo; 
@@ -29,13 +26,6 @@ import java.util.List;
     import cn.mandata.react_native_mpchart.MPChartPackage;
 
 public class MainApplication extends Application implements ReactApplication {
-
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
-
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -46,12 +36,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(mCallbackManager),
             new RNDeviceInfo(),
             new BackgroundTimerPackage(),
             new VectorIconsPackage(),
-            new RNSoundPackage(),
-            new MPChartPackage()
+            new RNSoundPackage()
       );
     }
   };
@@ -65,7 +53,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    FacebookSdk.sdkInitialize(getApplicationContext());
     // If you want to use AppEventsLogger to log events.
     AppEventsLogger.activateApp(this);
 
@@ -73,11 +60,7 @@ public class MainApplication extends Application implements ReactApplication {
             PackageInfo info = getPackageManager().getPackageInfo(
                     "com.silencesound", 
                     PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHashxxxxxxxxxxxxx:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-                }
+            
         } catch (NameNotFoundException e) {
 
         } catch (Exception e) {
